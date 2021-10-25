835b3fbae41825cf4b73b17118a31ba202cda60a
donaldcrane <donaldagbakoba@gmail.com>
2021-10-25 22:05:18 +0100

    finished build

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,16 +1,16 @@
-const config = require('../../config');
+require("dotenv").config();
 
 module.exports = {
   development: {
-    url: config.DEV_DATABASE_URL,
+    url: process.env.DEV_DATABASE_URL,
     dialect: "postgres",
   },
   test: {
-    url: config.TEST_DATABASE_URL,
+    url: process.env.TEST_DATABASE_URL,
     dialect: "postgres",
   },
   production: {
-    url: config.PROD_DATABASE_URL,
+    url: process.env.PROD_DATABASE_URL,
     dialect: "postgres",
   },
 };
